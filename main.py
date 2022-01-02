81b6a69693564f94adf8c389d445fcbdbb9f7bc7
Raphael Cordon <56551789+raphaelcordon@users.noreply.github.com>
2022-01-02 11:33:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pyScripts.italiano import ita
 from pyScripts.login import log
 from pyScripts.users import use
 
-from flask import Flask
+from flask import Flask, sessions
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -11,6 +11,8 @@ app.register_blueprint(ind)
 app.register_blueprint(ita)
 app.register_blueprint(log)
 app.register_blueprint(use)
+
+session_cookie = sessions.SecureCookieSessionInterface().get_cookie_secure(app)
 
 if __name__ == '__main__':
     app.run()
