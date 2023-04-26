a6a44e9b185c1e8e8967ee2400c766890f2ffa7a
Raphael Cordon <56551789+raphaelcordon@users.noreply.github.com>
2023-04-26 07:30:26 -0300

    Update base_repos.py

--- repository/verbItaliano/base_repos.py
+++ repository/verbItaliano/base_repos.py
@@ -4,11 +4,11 @@ import psycopg2
 class PostgreDB:
     def __init__(self):
         self.conn = psycopg2.connect(
-            host='ec2-54-73-110-26.eu-west-1.compute.amazonaws.com',
-            database='djs348j1mlldt',
-            user='mhjwehpsnygovo',
+            host='ec2-52-210-44-5.eu-west-1.compute.amazonaws.com',
+            database='d5cgs2efbk9f55',
+            user='pzenbabrskuqkz',
             port='5432',
-            password='45bb40f50f6b24e466ac36270fc2ecdedae29b03382b120a1b1af335ec273f4a'
+            password='f7cd719b111c1d5f8fc7c313482967c457bb855de1cc6619648b22624172a21a'
         )
         self.cur = self.conn.cursor()
 
